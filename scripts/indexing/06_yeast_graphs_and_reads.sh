11587b8436a67f84d7ea464405fd837757a29a27
Adam Novak <anovak@soe.ucsc.edu>
2020-09-18 17:23:44 -0700

    Change the reference over and produce gzipped FASTQs

--- scripts/indexing/06_yeast_graphs_and_reads.sh
+++ scripts/indexing/06_yeast_graphs_and_reads.sh
@@ -11,9 +11,9 @@ IN_SUBSET_HAL="/public/groups/cgl/users/daheller/yeast_graph/graphs/cactus_four/
 # What training FASTQ should be used for simulating reads?
 IN_TRAINING_FASTQ="/public/groups/cgl/users/daheller/yeast_graph/illumina_reads/SRR4074257.fastq.gz"
 # Where's the input reference FASTA for the linear control?
-IN_REFERENCE_FASTA="/public/groups/cgl/users/daheller/yeast_graph/assemblies/assemblies_raw/SK1.genome.fa"
+IN_REFERENCE_FASTA="/public/groups/cgl/users/daheller/yeast_graph/assemblies/assemblies_raw/S288C.genome.fa"
 # What strain is it?
-REFERENCE_STRAIN="SK1"
+REFERENCE_STRAIN="S288C"
 
 # Where are we?
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
@@ -129,19 +129,11 @@ if [ ! -e ${WORKDIR}/yeast_reference.fa.amb  ] ; then
     bwa index ${WORKDIR}/yeast_reference.fa
 fi
 
-for STRAIN in $(cat ${WORKDIR}/heldout_strains.txt) ; do
-    # Prepare reads
-    if [ ! -e  "${WORKDIR}/sim-${STRAIN}.gam" ] ; then
-        echo "Simulating reads for strain ${STRAIN}"
-        vg sim -t 10 -a -x ${WORKDIR}/yeast_all.vg -F "${IN_TRAINING_FASTQ}" -p 570 -v 165 -i 0.00029 -n 500000 $(cat ${WORKDIR}/all_contigs.txt | grep "${STRAIN}" | sed 's/^/--path /g' | tr '\n' ' ') | vg annotate -m -a - -x ${WORKDIR}/yeast_all.vg > "${WORKDIR}/sim-${STRAIN}.gam"
-    fi
-done
-
 for FILENAME in $(cd ${WORKDIR} && ls yeast_reference.*) ; do
     if [[ "${FILENAME#*.}" == ".mapping" || "${FILENAME#*.}" == ".pruned.vg" ]] ; then
         continue
     fi
-    aws s3 cp "${WORKDIR}/${FILENAME}" "s3://vg-k8s/profiling/graphs/v2/generic/primary/SK1/primarySK1.${FILENAME#*.}"
+    aws s3 cp "${WORKDIR}/${FILENAME}" "s3://vg-k8s/profiling/graphs/v2/generic/primary/${REFERENCE_STRAIN}/primary${REFERENCE_STRAIN}.${FILENAME#*.}"
 done
 for FILENAME in $(cd ${WORKDIR} && ls yeast_all.*) ; do
     if [[ "${FILENAME#*.}" == ".mapping" || "${FILENAME#*.}" == ".pruned.vg" ]] ; then
@@ -155,6 +147,26 @@ for FILENAME in $(cd ${WORKDIR} && ls yeast_subset.*) ; do
     fi
     aws s3 cp "${WORKDIR}/${FILENAME}" "s3://vg-k8s/profiling/graphs/v2/generic/cactus/yeast_subset/${FILENAME}"
 done
+
+for STRAIN in $(cat ${WORKDIR}/heldout_strains.txt) ; do
+    # Prepare reads
+    if [ ! -e  "${WORKDIR}/sim-${STRAIN}.gam" ] ; then
+        echo "Simulating reads for strain ${STRAIN}"
+        vg sim -t 10 -a -x ${WORKDIR}/yeast_all.vg -F "${IN_TRAINING_FASTQ}" -p 570 -v 165 -i 0.00029 -n 500000 $(cat ${WORKDIR}/all_contigs.txt | grep "${STRAIN}" | sed 's/^/--path /g' | tr '\n' ' ') | vg annotate -m -a - -x ${WORKDIR}/yeast_all.vg > "${WORKDIR}/sim-${STRAIN}.gam" &
+    fi
+done
+
+barrier
+
+for STRAIN in $(cat ${WORKDIR}/heldout_strains.txt) ; do
+    if [ ! -e  "${WORKDIR}/sim-${STRAIN}.fq.gz" ] ; then
+        # Keep vg pairing style
+        (vg view -i -X "${WORKDIR}/sim-${STRAIN}.gam" > "${WORKDIR}/sim-${STRAIN}.fq" && gzip "${WORKDIR}/sim-${STRAIN}.fq") &
+    fi
+done
+
+barrier
+
 for FILENAME in $(cd ${WORKDIR} && ls sim-*) ; do
     aws s3 cp "${WORKDIR}/${FILENAME}" "s3://vg-k8s/profiling/reads/sim/yeast/${FILENAME}"
 done
