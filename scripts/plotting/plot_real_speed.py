ab93c394ccb5834aedf0a299901351cf1774bda8
Xian Chang <xhchang15@gmail.com>
2020-10-23 19:43:01 -0400

    Update speed plotting

--- scripts/plotting/plot_real_speed.py
+++ scripts/plotting/plot_real_speed.py
@@ -7,8 +7,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import argparse
 
-#Plot speed, memory, and runtime for 1kg, hgsvc, and yeast
-#Input file (speed_report_all.tsv) must be formatted:
+#Plot speed, memory, and runtime
+#Input file is must be formatted:
 #graph   algorithm   reads   pairing speed   total_cpu_sec   total_wall_clock_time   max_resident_set_size(kbytes)
 
 matplotlib.rcParams["font.family"]="sans-serif"
@@ -19,25 +19,42 @@ matplotlib.rcParams['axes.titlesize']="x-large"
 matplotlib.rcParams['axes.labelsize']="large"
 matplotlib.rcParams['axes.labelcolor'] = "black"
 
-colors_single = { "bowtie2":"#cccc22", "bwa_mem":"#ffee99", "giraffe":"#55ccaa", "giraffe_fast":"#99ddff", "giraffe_primary":"#88bbcc", "graphaligner":"#ccdd44", "hisat2":"#ff9977", "vg_map":"#ffbbcc", "minimap2":"#eaeaea"}
-colors_original={ "bowtie2":"#aaaa00", "bwa_mem":"#eedd88", "giraffe":"#44bb99", "giraffe_fast":"#99ddff", "giraffe_primary":"#77aadd", "graphaligner":"#bbcc33", "hisat2":"#ee8866", "vg_map":"#ffaabb", "minimap2":"#dddddd"}
-colors_paired = { "bowtie2":"#aaaa00", "bwa_mem":"#ddcc77", "giraffe":"#33aa88", "giraffe_fast":"#77bbdd", "giraffe_primary":"#6699cc", "graphaligner":"#aabb22", "hisat2":"#dd7755", "vg_map":"#ee99aa", "minimap2":"#cccccc"}
+#(light, original, dark)
+colors={ "bowtie2"         : ("#cccc22", "#aaaa00", "#aaaa00"), 
+         "bwa_mem"         : ("#ffee99", "#eedd88", "#ddcc77"),  
+         "minimap2"        : ("#eaeaea", "#dddddd", "#cccccc"),
+         "giraffe"         : ("#66ddbb", "#44bb99", "#44bb99"), 
+         "giraffe_cover"   : ("#66ddbb", "#44bb99", "#44bb99"), 
+         "giraffe_sampled" : ("#66ddbb", "#44bb99", "#44bb99"), 
+         "giraffe_full"    : ("#007755", "#118866", "#229977"),
+         "giraffe_fast"    : ("#99ddff", "#99ddff", "#77bbdd"), 
+         "giraffe_primary" : ("#88bbcc", "#77aadd", "#6699cc"),
+         "graphaligner"    : ("#ccdd44", "#bbcc33", "#aabb22"), 
+         "vg_map"          : ("#ffbbcc", "#ffaabb", "#ee99aa"),
+         "hisat2"          : ("#ff9988", "#ee8877", "#dd7766"),
+         "hisat2_sens"     : ("#cc6655", "#bb5544", "#aa4433"),
+         "hisat2_vsens"    : ("#993322", "#882211", "#771100")}
 
 fix_names = {"giraffe"         : "vg giraffe", 
-             "giraffe_fast"    : " giraffe fast", 
+             "giraffe_fast"    : "giraffe fast", 
              "giraffe_primary" : "giraffe primary",
+             "giraffe_sampled" : "giraffe sampled",
+             "giraffe_cover"   : "giraffe cover",
+             "giraffe_full"    : "giraffe full",
              "vg_map"          : "vg map",
              "minimap2"        : "minimap2", 
              "bwa_mem"         : "bwa mem",
              "bowtie2"         : "bowtie2",
              "graphaligner"    : "graphaligner",
-             "hisat2"          : "hisat2"}
+             "hisat2"          : "hisat2",
+             "hisat2_sens"     : "hisat2 sensitive",
+             "hisat2_vsens"    : "hisat2 very sensitive"}
 
-def plot_histogram(stat_name, graph_name, title_name, data, units):
+def plot_histogram(stat_name, graph_name, title_name, data, units, read_name):
     #data maps {mapper_name : (single val, paired val)}
     
     fig_width = 14
-    fig_height = 10
+    fig_height = 15
     plt.figure(figsize = (fig_width, fig_height))
     
     panel_width = .8
@@ -50,22 +67,30 @@ def plot_histogram(stat_name, graph_name, title_name, data, units):
     mapper_names = list(data.keys())
 
     #[ (value, mapper, is_paired)]
-    sorted_vals = sorted(filter(lambda x : x[0] != 0 , [( data[mapper_names[i]][0],mapper_names[i], False) for i in range(len(mapper_names))] + 
-                         [( data[mapper_names[i]][1],mapper_names[i], True) for i in range(len(mapper_names))])) 
+    sorted_vals = sorted(filter(lambda x : x[0] != 0 , 
+        [( data[mapper_names[i]][0],data[mapper_names[i]][2],mapper_names[i], False) for i in range(len(mapper_names))] + 
+        [( data[mapper_names[i]][1],data[mapper_names[i]][3],mapper_names[i], True) for i in range(len(mapper_names))])) 
 
 
     for i in range(len(sorted_vals)) :
-        (val,mapper,  is_paired) = sorted_vals[i]
+        (val,extra_val,mapper,  is_paired) = sorted_vals[i]
         y_start = i 
-        max_val = max(max_val, val)
-        color = colors_paired[mapper] if is_paired else colors_single[mapper]
-        paired_str = " paired" if is_paired else " single"
+        #if extra_val != 0:
+        #    panel.add_patch(mplpatches.Rectangle([val, y_start+0.05], extra_val, 0.9, linewidth=0, color='#bb5533'))
+
+        if (stat_name == "Memory") and (val > 240):
+            #Hacky way of cutting off the memory when we ran out
+
+            panel.text(0.5, y_start+0.5, "Out of Memory", verticalalignment="center", fontsize=15) 
+        else :
+            max_val = max(max_val, val)
+            color = colors[mapper][2] if is_paired else colors[mapper][0]
+            paired_str = " paired" if is_paired else " single"
     
-        panel.add_patch(mplpatches.Rectangle([0, y_start+0.05], val, 0.9, linewidth=0, color=color, label=fix_names.get(mapper, mapper)+paired_str))
-        #panel.text(val_single[0], y_start+0.5, "%.2f" % val_single[0], verticalalignment="center") 
+            panel.add_patch(mplpatches.Rectangle([0, y_start+0.05], val, 0.9, linewidth=0, color=color, label=fix_names.get(mapper, mapper)+paired_str))
         ylabels.append(fix_names.get(mapper, mapper)+paired_str)
     
-    panel.legend(loc="lower right")
+    #panel.legend(loc="lower right")
     
     
     panel.tick_params(axis='both', which='both',\
@@ -84,10 +109,10 @@ def plot_histogram(stat_name, graph_name, title_name, data, units):
     panel.set_title(title_name + " " + stat_name)
     
     #plt.show()
-    plt.savefig(graph_name + "_" + stat_name+"_plot.svg", dpi=400)
+    plt.savefig(graph_name + "_" + stat_name+"_"+read_name+"_plot.svg", dpi=400)
 
-def main():
-    with open("speed_report_all.tsv", "r") as infile:
+def main(read_name):
+    with open("speed_report_"+read_name+".tsv", "r") as infile:
         labels = infile.readline().split()
     
         #{graph : {mapper name : (single, paired)}} 
@@ -104,25 +129,35 @@ def main():
             speed = float(l[4])
             total_sec = float(l[5])
             time_str = l[6].split(":")
-            time = 0 if len(time_str) == 1 else float(time_str[0]) + (float(time_str[1])/60.0) + (float(time_str[2])/60.0) 
+            if len(time_str) == 2:
+                time_str = ["0"] + time_str
+            time = 0 if len(time_str) == 1 else float(time_str[0]) + (float(time_str[1])/60.0) + (float(time_str[2])/3600.0) 
             memory = float(l[7]) / 1000000.0
+            if len(l) == 9:
+                time_str = l[8].split(":")
+                extra_time = 0 if len(time_str) == 1 else float(time_str[0]) + (float(time_str[1])/60.0) + (float(time_str[2])/3600.0) 
+            else:
+                extra_time=0
+
         
-            old_speeds = all_speeds.get(graph, {}).get(mapper, (0,0))
-            old_times =   all_times.get(graph, {}).get(mapper, (0,0))
-            old_memory = all_memory.get(graph, {}).get(mapper, (0,0))
-            new_speeds = (speed, old_speeds[1])  if pairing == "single" else (old_speeds[0], speed)
-            new_times =  (time, old_times[1])    if pairing == "single" else (old_times[0], time)
-            new_memory = (memory, old_memory[1]) if pairing == "single" else (old_memory[0], memory)
+            #(single val, paired val, extra val single, extra val paired)
+            old_speeds = all_speeds.get(graph, {}).get(mapper, (0,0,0,0))
+            old_times =   all_times.get(graph, {}).get(mapper, (0,0,0,0))
+            old_memory = all_memory.get(graph, {}).get(mapper, (0,0,0,0))
+            new_speeds = (speed, old_speeds[1],0,0)  if pairing == "single" else (old_speeds[0], speed,0,0)
+            new_times =  (time, old_times[1],extra_time, old_times[3])  if pairing == "single" else (old_times[0], time,old_times[2], extra_time)
+            new_memory = (memory, old_memory[1],0,0) if pairing == "single" else (old_memory[0], memory,0,0)
             all_speeds[graph][mapper] = new_speeds
             all_times [graph][mapper]  = new_times
             all_memory[graph][mapper] = new_memory
     
 
-    plot_histogram("Speed",  "1kg", "1KGP/GRCh37", all_speeds["1kg"], "reads/second/thread")
-    plot_histogram("Runtime","1kg", "1KGP/GRCh37", all_times["1kg"],  "hours")
-    plot_histogram("Memory", "1kg", "1KGP/GRCh37", all_memory["1kg"], "gbytes")
-    plot_histogram("Speed",  "hgsvc", "HGSVC/GRCh38", all_speeds["hgsvc"], "reads/second/thread")
-    plot_histogram("Runtime","hgsvc", "HGSVC/GRCh38", all_times ["hgsvc"],  "hours")
-    plot_histogram("Memory", "hgsvc", "HGSVC/GRCh38", all_memory["hgsvc"], "gbytes")
-    plot_histogram("Speed",  "yeast", "yeast", all_speeds["yeast"], "reads/second/thread")
-main()
+    plot_histogram("Speed",  "1kg", "1KGP/GRCh37", all_speeds["1kg"], "reads/second/thread", read_name)
+    plot_histogram("Runtime","1kg", "1KGP/GRCh37", all_times["1kg"],  "hours", read_name)
+    plot_histogram("Memory", "1kg", "1KGP/GRCh37", all_memory["1kg"], "gbytes", read_name)
+    plot_histogram("Speed",  "hgsvc", "HGSVC/GRCh38", all_speeds["hgsvc"], "reads/second/thread", read_name)
+    plot_histogram("Runtime","hgsvc", "HGSVC/GRCh38", all_times ["hgsvc"],  "hours", read_name)
+    plot_histogram("Memory", "hgsvc", "HGSVC/GRCh38", all_memory["hgsvc"], "gbytes", read_name)
+    plot_histogram("Speed",  "yeast", "yeast", all_speeds["yeast"], "reads/second/thread", read_name)
+
+main("novaseq600")
