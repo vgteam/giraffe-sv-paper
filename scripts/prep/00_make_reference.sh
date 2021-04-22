a681765832f4f6a8484fffee9137858a276f0c93
Adam Novak <anovak@soe.ucsc.edu>
2021-04-22 10:14:36 -0700

    Add build of renamed reference

--- scripts/prep/00_make_reference.sh
+++ scripts/prep/00_make_reference.sh
@@ -5,7 +5,13 @@ rm -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz GCA_000786075.2_hs38d1_
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz            
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/786/075/GCA_000786075.2_hs38d1/GCA_000786075.2_hs38d1_genomic.fna.gz
 zcat GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz GCA_000786075.2_hs38d1_genomic.fna.gz > GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.fna
+
+cat GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.fna | sed -r -e 's/>chr([0-9XYMT]+) />\1 /g' >GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.renamed.fna
+
 gzip GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.fna
 aws s3 cp GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.fna.gz s3://vg-k8s/profiling/data/GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.fna.gz
+
+pbgzip GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.renamed.fna
+aws s3 cp GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.renamed.fna.gz s3://vg-k8s/profiling/data/GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.renamed.fna.gz
             
 
