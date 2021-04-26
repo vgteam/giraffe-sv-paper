8a854bf0244d504143394a868538045e32c32d42
Adam Novak <anovak@soe.ucsc.edu>
2021-04-26 14:42:08 -0700

    Remove extra indent

--- scripts/archiving/archive_code_and_containers.sh
+++ scripts/archiving/archive_code_and_containers.sh
@@ -218,11 +218,11 @@ if [[ ! -z "${ZENODO_DEPOSITION}" && ! -z "${ZENODO_TOKEN}" ]] ; then
     export FILEPATH="${SOFTWARE_ZIP_FILE}"
     # Upload the completed zip file onto the Zenodo deposition specified by the environment
     python3 -c 'import requests; 
-    import os; deposition=os.environ["ZENODO_DEPOSITION"]; 
-    filepath=os.environ["FILEPATH"]; 
-    filename=os.path.basename(filepath); 
-    params={"access_token": os.environ["ZENODO_TOKEN"]}; 
-    bucket=requests.get(f"https://www.zenodo.org/api/deposit/depositions/{deposition}", params=params).json()["links"]["bucket"]; 
-    requests.put(f"{bucket}/{filename}", data=open(filepath, "rb"), params=params).raise_for_status();'
+import os; deposition=os.environ["ZENODO_DEPOSITION"]; 
+filepath=os.environ["FILEPATH"]; 
+filename=os.path.basename(filepath); 
+params={"access_token": os.environ["ZENODO_TOKEN"]}; 
+bucket=requests.get(f"https://www.zenodo.org/api/deposit/depositions/{deposition}", params=params).json()["links"]["bucket"]; 
+requests.put(f"{bucket}/{filename}", data=open(filepath, "rb"), params=params).raise_for_status();'
 fi
 
