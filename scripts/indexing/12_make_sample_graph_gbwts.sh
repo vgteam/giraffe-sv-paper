cb7726ba170bbed2507a1cd2c539653ae3a5d16a
Adam Novak <anovak@soe.ucsc.edu>
2021-05-03 12:22:07 -0700

    Drop HGSVC NA19240_sample graph in favor of sample-only GBWT

--- scripts/indexing/12_make_sample_graph_gbwts.sh
+++ scripts/indexing/12_make_sample_graph_gbwts.sh
@@ -24,12 +24,12 @@ if [[ ! -e HGSVC_hs38d1.onlyNA19240.augment.gbwt ]] ; then
 fi
 aws s3 cp HGSVC_hs38d1.onlyNA19240.augment.gbwt s3://vg-k8s/profiling/graphs/v2/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1.onlyNA19240.augment.gbwt
 
+
+# Now do a sample graph GBWT for 1000GPlons
+
 # Download everything
 if [[ ! -e 1000GPlons_hs38d1_NA19239_sample_withref.vg ]] ; then
     aws s3 cp s3://vg-k8s/profiling/graphs/v3/for-NA19239/1000gplons/hs38d1/1000GPlons_hs38d1_NA19239_sample_withref.vg .
-fi
-if [[ ! -e HGSVC_hs38d1_NA19240_sample_withref.vg ]] ; then
-    aws s3 cp s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1_NA19240_sample_withref.vg .
 fi
 
 if [[ ! -e 1000GPlons_hs38d1_NA19239_sample_withref.xg ]] ; then
@@ -38,14 +38,6 @@ fi
 
 if [[ ! -e 1000GPlons_hs38d1_NA19239_sample_withref.force.gbwt ]] ; then
     aws s3 cp s3://vg-k8s/profiling/graphs/v3/for-NA19239/1000gplons/hs38d1/1000GPlons_hs38d1_NA19239_sample_withref.force.gbwt . || true
-fi
-
-if [[ ! -e HGSVC_hs38d1_NA19240_sample_withref.xg ]] ; then
-    aws s3 cp s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1_NA19240_sample_withref.xg . || true
-fi
-
-if [[ ! -e HGSVC_hs38d1_NA19240_sample_withref.force.gbwt ]] ; then
-    aws s3 cp s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1_NA19240_sample_withref.force.gbwt . || true
 fi
 
 CHROM_OPTS=()
@@ -58,15 +50,6 @@ for CHROM in {1..22} X Y ; do
     fi
     CHROM_OPTS+=(ALL.chr${CHROM}_GRCh38.genotypes.20170504_NA19239.vcf.gz)
 done
-
-if [[ ! -e HGSVC.haps_NA19240.vcf.gz ]] ; then
-    aws s3 cp s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1-vcfs/HGSVC.haps_NA19240.vcf.gz .
-fi
-
-if [[ ! -e HGSVC.haps_NA19240.vcf.gz.tbi ]] ; then
-    aws s3 cp s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1-vcfs/HGSVC.haps_NA19240.vcf.gz.tbi .
-fi
-
 
 # Index 1000GPlons sample graph
 
@@ -84,28 +67,11 @@ if [[ ! -e 1000GPlons_hs38d1_NA19239_sample_withref.force.augment.gg || ! -e 100
 fi
 
 
-# Index HGSVC sample graph
-
-if [[ ! -e HGSVC_hs38d1_NA19240_sample_withref.force.gbwt || ! -e HGSVC_hs38d1_NA19240_sample_withref.xg ]] ; then
-    vg index --force-phasing --discard-overlaps -x HGSVC_hs38d1_NA19240_sample_withref.xg -G HGSVC_hs38d1_NA19240_sample_withref.force.gbwt -v HGSVC.haps_NA19240.vcf.gz HGSVC_hs38d1_NA19240_sample_withref.vg
-fi
-
-# If we use a local haplotype cover we will introduce switch errors; augment instead.
-if [[ ! -e HGSVC_hs38d1_NA19240_sample_withref.force.augment.gg || ! -e HGSVC_hs38d1_NA19240_sample_withref.force.augment.gbwt ]] ; then
-    vg gbwt -p -g HGSVC_hs38d1_NA19240_sample_withref.force.augment.gg -o HGSVC_hs38d1_NA19240_sample_withref.force.augment.gbwt -x HGSVC_hs38d1_NA19240_sample_withref.xg -a HGSVC_hs38d1_NA19240_sample_withref.force.gbwt 
-fi
-
 # (Re)Upload everything
 aws s3 cp 1000GPlons_hs38d1_NA19239_sample_withref.xg s3://vg-k8s/profiling/graphs/v3/for-NA19239/1000gplons/hs38d1/1000GPlons_hs38d1_NA19239_sample_withref.xg
 aws s3 cp 1000GPlons_hs38d1_NA19239_sample_withref.force.gbwt s3://vg-k8s/profiling/graphs/v3/for-NA19239/1000gplons/hs38d1/1000GPlons_hs38d1_NA19239_sample_withref.force.gbwt
 aws s3 cp 1000GPlons_hs38d1_NA19239_sample_withref.force.augment.gbwt s3://vg-k8s/profiling/graphs/v3/for-NA19239/1000gplons/hs38d1/1000GPlons_hs38d1_NA19239_sample_withref.force.augment.gbwt
 aws s3 cp 1000GPlons_hs38d1_NA19239_sample_withref.force.augment.gg s3://vg-k8s/profiling/graphs/v3/for-NA19239/1000gplons/hs38d1/1000GPlons_hs38d1_NA19239_sample_withref.force.augment.gg
-
-aws s3 cp HGSVC_hs38d1_NA19240_sample_withref.xg s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1_NA19240_sample_withref.xg
-aws s3 cp HGSVC_hs38d1_NA19240_sample_withref.force.gbwt s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1_NA19240_sample_withref.force.gbwt
-aws s3 cp HGSVC_hs38d1_NA19240_sample_withref.force.augment.gbwt s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1_NA19240_sample_withref.force.augment.gbwt
-aws s3 cp HGSVC_hs38d1_NA19240_sample_withref.force.augment.gg s3://vg-k8s/profiling/graphs/v3/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1_NA19240_sample_withref.force.augment.gg
-
 
             
               
